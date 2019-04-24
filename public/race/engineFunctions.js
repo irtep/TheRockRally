3050a3cbf3a0718e2f2c961aee18627426f46ffd
Glitch (therockrally) <none>
2019-04-24 10:01:33 +0000

    update 24apr19 midday

--- public/race/engineFunctions.js
+++ public/race/engineFunctions.js
@@ -49,27 +49,27 @@ function checkKeyPressed(pressed){
   
     // up  
     case 'ArrowUp': 
-      car1.statuses.accelerate = true;
+      gameObject.race[0].statuses.accelerate = true;
     break;
     
     // down
     case 'ArrowDown': 
-      car1.statuses.braking = true;
+      gameObject.race[0].statuses.braking = true;
     break;
       
     // left  
     case 'ArrowLeft': 
-      car1.statuses.turnLeft = true;
+      gameObject.race[0].statuses.turnLeft = true;
     break;
       
     // right  
     case 'ArrowRight': 
-      car1.statuses.turnRight = true;
+      gameObject.race[0].statuses.turnRight = true;
     break;
       
     // 'r' for reverse:
     case 'KeyR':
-      car1.statuses.reverse = true;  
+      gameObject.race[0].statuses.reverse = true;  
     break;
       
     default: console.log('not found this key(pressed)');  
@@ -81,33 +81,78 @@ function checkKeyReleased(released){
   
     // up  
     case 'ArrowUp': 
-      car1.statuses.accelerate = false;
+      gameObject.race[0].statuses.accelerate = false;
     break;
     
     // down
     case 'ArrowDown': 
-      car1.statuses.braking = false;
+      gameObject.race[0].statuses.braking = false;
     break;
       
     // left  
     case 'ArrowLeft': 
-      car1.statuses.turnLeft = false;
+      gameObject.race[0].statuses.turnLeft = false;
     break;
       
     // right  
     case 'ArrowRight': 
-      car1.statuses.turnRight = false;
+      gameObject.race[0].statuses.turnRight = false;
     break;      
     // 'r' for reverse:
     case 'KeyR':
-      car1.statuses.reverse = false;  
+      gameObject.race[0].statuses.reverse = false;  
     break;
       
     default: console.log('not found this key(released) ');  
   }
 }
 
+function updateCar(carOnCase) {
 
+  carOnCase.weight = carOnCase.chassis.weight + carOnCase.armour.weight + carOnCase.motor.weight;
+  carOnCase.cost = carOnCase.chassis.cost + carOnCase.armour.cost + carOnCase.tires.cost + carOnCase.motor.cost;
+  carOnCase.statuses.power = carOnCase.motor.power - (carOnCase.weight/10);
+  carOnCase.statuses.maxSpeed = carOnCase.motor.maxSpeed - carOnCase.weight;
+  carOnCase.statuses.grip = carOnCase.tires.grip - carOnCase.weight;
+  carOnCase.pieces.hull.color = carOnCase.color;
+  
+  return carOnCase;
+}
+
+function createNewCar(newCar){
+//  car: {driver: null, name: null, color: null, chassis: null, motor: null, tires: null, armour: null, pieces: null, statuses: null}
+  // search chassis, motor, tires, armour and pieces by cars name:
+  
+  const stats = vehicles.filter( (cars) => newCar.name === cars.name);
+  
+  // chassis has pieces
+  const chassis = chassises.filter( (chas) => stats[0].chassis === chas.name);
+  
+  const mot = motors.filter( (moto) => stats[0].motor === moto.name);
+  const tire = tires.filter( (tir) => stats[0].tires === tir.name);
+  const armour = armours.filter( (armo) => stats[0].armour === armo.name);
+  
+  newCar.chassis = chassis[0]; newCar.motor = mot[0]; newCar.tires = tire[0]; newCar.armour = armour[0];
+  newCar.pieces = chassis[0].pieces;
+  
+  newCar = updateCar(newCar);
+  
+  //console.log('car: ', newCar);
+  // need to fix that below... doesnt add cost and weight! weight at least is mandatory
+  gameObject.race.push(new Car(newCar.name, newCar.pieces, newCar.statuses));
+  console.log('gameObject ', gameObject);
+}
+
+function setupRace(){
+  // ok, i need atleast: statuses: power, maxSpeed, turnRate, grip, weight, armour.. all else to default values
+  
+  // players car:
+  createNewCar(gameObject.car);
+  // ai cars:
+
+}
+
+/*
 
 function rotatePoint (coords, angle, distance) {
   const TO_RADIANS = Math.PI/180;
@@ -127,7 +172,7 @@ function distance (from, to) {
 		;
 	return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))
 }
-
+*/
 /**
  * Provides requestAnimationFrame in a cross browser way.
  * http://paulirish.com/2011/requestanimationframe-for-smart-animating/
